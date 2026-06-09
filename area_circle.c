7f602b3321a6a7c6186e9534a44b22e8ec5357f7
preetha308 <preethaprakash378@gmail.com>
2026-06-09 08:23:10 +0530

    Delete area_circle.c

--- area_circle.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int radius;
-    printf("Enter radius : ");
-    scanf("%d", &radius);
-    printf("Area of the circle is : %f", 3.14*radius*radius);
-    return 0;
-}
